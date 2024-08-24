3c681c83e03cd7610a87b5320f7ef1f1a4666b5a
Baran <baranzengeralp@gmail.com>
2024-08-24 23:54:26 +0300

    commit

--- xylophone/ViewController.swift
+++ xylophone/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  xylophone
-//
-//  Created by Baran Zengeralp on 24.08.24.
-//
-
 import AVFoundation
 import UIKit
 
@@ -12,38 +5,36 @@ class ViewController: UIViewController {
     private var buttonList: [UIButton] = []
     private let buttonTextList = ["C", "D", "E", "F", "G", "A", "B"]
     private let buttonUIColorList: [UIColor] = [.red, .orange, .yellow, .green, .systemBlue, .blue, .purple]
-    private var player: AVAudioPlayer?
+    private var audioPlayers: [String: AVAudioPlayer] = [:] // Dictionary to store preloaded audio players
     private let feedbackGenerator = UIImpactFeedbackGenerator()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         view.backgroundColor = UIColor(hex: "FFFFFF")
         setupStackViewWithButtons()
+        preloadSounds() // Preload sounds in viewDidLoad
     }
-
+    
     public func setupStackViewWithButtons() {
         let stackView = createStackView()
-
         view.addSubview(stackView)
-
         setupStackViewConstraints(stackView: stackView)
-
-        for i in 0 ... 6 {
+        
+        for i in 0...6 {
             let button = createButton(title: buttonTextList[i], color: buttonUIColorList[i])
             let view = createUIView(index: i, button: button)
             buttonList.append(button)
             stackView.addArrangedSubview(view)
         }
     }
-
+    
     public func createUIView(index: Int, button: UIButton) -> UIView {
         let uiView = UIView()
         button.translatesAutoresizingMaskIntoConstraints = false
-        uiView.backgroundColor = .clear // Görünüm arka plan rengini ayarlayabilirsiniz
-
+        uiView.backgroundColor = .clear
+        
         uiView.addSubview(button)
-
+        
         NSLayoutConstraint.activate([
             button.centerXAnchor.constraint(equalTo: uiView.centerXAnchor),
             button.centerYAnchor.constraint(equalTo: uiView.centerYAnchor),
@@ -54,7 +45,7 @@ class ViewController: UIViewController {
         ])
         return uiView
     }
-
+    
     public func createStackView() -> UIStackView {
         let stackView = UIStackView()
         stackView.axis = .vertical
@@ -63,7 +54,7 @@ class ViewController: UIViewController {
         stackView.spacing = 30
         return stackView
     }
-
+    
     public func createButton(title: String, color: UIColor) -> UIButton {
         let button = UIButton(type: .system)
         button.setTitle(title, for: .normal)
@@ -71,15 +62,13 @@ class ViewController: UIViewController {
         button.layer.cornerRadius = 20
         button.tintColor = .white
         button.titleLabel?.font = UIFont(name: "Bradley Hand", size: 45)
-
         button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
-
         return button
     }
-
+    
     public func setupStackViewConstraints(stackView: UIStackView) {
         stackView.translatesAutoresizingMaskIntoConstraints = false
-
+        
         NSLayoutConstraint.activate([
             stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
@@ -87,18 +76,33 @@ class ViewController: UIViewController {
             stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
         ])
     }
-
-    public func playSound(_ title: String) {
-        let url = Bundle.main.url(forResource: title, withExtension: "wav")
-        player = try! AVAudioPlayer(contentsOf: url!)
-        player?.play()
+    
+    public func preloadSounds() {
+        for sound in buttonTextList {
+            if let url = Bundle.main.url(forResource: sound, withExtension: "wav") {
+                do {
+                    let player = try AVAudioPlayer(contentsOf: url)
+                    player.prepareToPlay()
+                    audioPlayers[sound] = player // Store the preloaded player in the dictionary
+                } catch {
+                    print("Error loading sound \(sound): \(error.localizedDescription)")
+                }
+            }
+        }
     }
-
+    
+    public func playSound(_ title: String) {
+        if let player = audioPlayers[title] {
+            player.currentTime = 0 // Reset the player to the beginning
+            player.play()
+        }
+    }
+    
     @objc private func buttonPressed(_ sender: UIButton) {
-        if buttonList.firstIndex(of: sender) != nil {
-            print("Button \(sender.currentTitle!) tapped")
+        if let title = sender.currentTitle {
+            print("Button \(title) tapped")
             feedbackGenerator.impactOccurred()
-            playSound(sender.currentTitle!)
+            playSound(title)
         }
     }
 }
@@ -107,15 +111,15 @@ extension UIColor {
     convenience init(hex: String) {
         var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
         hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
-
+        
         var rgb: UInt64 = 0
         Scanner(string: hexSanitized).scanHexInt64(&rgb)
-
+        
         let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
         let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
         let blue = CGFloat(rgb & 0x0000FF) / 255.0
         let alpha = CGFloat(1.0)
-
+        
         self.init(red: red, green: green, blue: blue, alpha: alpha)
     }
 }
